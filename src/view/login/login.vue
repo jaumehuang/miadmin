76c40f3bbc600c5b83eb21151ac78b3cfbd1f7f7
jaumehaung <1332204564@qq.com>
2018-07-13 11:50:37 +0800

    登陆界面

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -3,32 +3,32 @@
          <!-- logo -->
          <!-- <img class="logo-img" src="../../assets/logo.png"> -->
          <el-form   class="login-form-inline" label-width="80px">
-              <el-row class="title">
-                  <el-col :span="24"><div class="grid-content bg-purple-dark">后台管理系统</div></el-col>
-             </el-row>
-              <el-form-item label="账号" :inline="true">
-                <el-input  placeholder="请输入账号" class="input-item" v-model="name"  required=""></el-input>
-              </el-form-item>
-              <el-form-item label="密码" :inline="true">
-                <el-input  placeholder="请输入密码" class="input-item" v-model="password" required=""></el-input>
-              </el-form-item>
-              <el-form-item label="验证码" :inline="true">
-                <el-input  placeholder="请输入验证码" class="input-item"></el-input>
-              </el-form-item>
+            <el-row>
+                <el-row class="title">
+                    <el-col :span="24"><div class="grid-content bg-purple-dark">后台管理系统</div></el-col>
+              </el-row>
+                <el-form-item label="账号" :inline="true">
+                  <el-input  placeholder="请输入账号" class="input-item" ></el-input>
+                </el-form-item>
+                <el-form-item label="密码" :inline="true">
+                  <el-input  placeholder="请输入密码" class="input-item" ></el-input>
+                </el-form-item>
+                <el-form-item label="验证码" :inline="true">
+                  <el-input  placeholder="请输入验证码" class="input-item"></el-input>
+                </el-form-item>
+            </el-row>
+             <el-row  class="warm-title">注：仅供测试使用</el-row>
               <el-button type="primary" class="btn-login">登录</el-button>
          </el-form>
      </div>
 </template>
 <script>
 export default {
-    data(){
-         
-        return {
-                        
-        }
-    }
+  data() {
+    return {};
   }
-</script>
+};
+</script> 
 
 <style lang="scss">
 .login {
@@ -44,31 +44,41 @@ export default {
     left: 0;
     top: 0;
   }
-  .login-form-inline{
-    
-    padding-bottom:0;
+  .login-form-inline {
+    height: 340px;
+    width: 340px;
+    padding-bottom: 0;
     position: absolute;
-    left:50%;
-    top:50%;
-    transform: translate(-50%,-50%);
-    background-color:#fff;
-    margin-bottom:10px;
-        .title{
-            text-align: center;
-            height:60px;
-            line-height: 60px;
-            color:#fff;
-            background-color:#409eff;
-
-        }
-        .btn-login{
-            width:50%;
-        }
-        .input-item{
-            width:400px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    margin-bottom: 10px;
+    margin: auto;
+    .el-form-item {
+      padding-right: 20px;
+      margin-top: 20px;
+    }
+    .title {
+      height: 60px;
+      line-height: 60px;
+      text-align: center;
+      color: #fff;
+      background-color: #409eff;
+    }
+    .warm-title{
+      
+      padding-left:24px;
+    }
+    .btn-login {
+      width: 300px;
+      position: absolute;
+      bottom: 10px;
+      left: 50%;
+      transform: translateX(-50%);
+      -webkit-transform: translateX(-50%);
     }
   }
-  
 }
 </style>
 
